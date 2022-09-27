0d4185920b73ce0c6a4170fbc98b8fcde7b76a1d
Suraj Megharaj <87960243+SurajMegharaj@users.noreply.github.com>
2022-09-27 07:35:44 +0530

    Create Array2.java

---
+++ Array2.java
@@ -0,0 +1,15 @@
+public class New{
+    public static void main(String[] args){
+
+        int[] arr = {4,23,5,2,4};
+        for(int i=4;i>=0;i--){
+            System.out.print(arr[i]);
+
+            
+        }
+      
+ 
+
+    }
+
+}
